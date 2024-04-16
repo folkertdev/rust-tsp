cfcabd0a34ad04839e096b228c7e67dfb1180174
Marlon Baeten <marlon@tweedegolf.com>
2024-04-16 12:32:11 +0200

    Added example to tsp crate

--- tsp/src/lib.rs
+++ tsp/src/lib.rs
@@ -1,3 +1,53 @@
+//! # Trust Spanning Protocol
+//!
+//! The Trust Spanning Protocol (TSP) is a protocol for secure communication
+//! between entities identified by their Verified Identities (VID's).
+//!
+//! The primary API this crates exposes is the [VidDatabase] struct, which
+//! is used to manage and resolve VID's, as well as send and receive messages
+//! between them.
+//!
+//! # Example
+//!
+//! The following example demonstrates how to send a message from Alice to Bob
+//!
+//! ```no_run
+//! use tsp::{VidDatabase, PrivateVid, Error, ReceivedTspMessage};
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<(), Error> {
+//!     // bob database
+//!     let mut bob_db = VidDatabase::new();
+//!     bob_db.add_private_vid_from_file("test/bob.json").await?;
+//!     bob_db.verify_vid("did:web:did.tsp-test.org:user:alice").await?;
+//!
+//!     let mut bobs_messages = bob_db.receive("did:web:did.tsp-test.org:user:bob").await?;
+//!
+//!     // alice database
+//!     let mut alice_db = VidDatabase::new();
+//!     alice_db.add_private_vid_from_file("test/alice.json").await?;
+//!     alice_db.verify_vid("did:web:did.tsp-test.org:user:bob").await?;
+//!
+//!     // send a message
+//!     alice_db.send(
+//!         "did:web:did.tsp-test.org:user:alice",
+//!         "did:web:did.tsp-test.org:user:bob",
+//!         Some(b"extra non-confidential data"),
+//!         b"hello world",
+//!     ).await?;
+//!
+//!     // receive a message
+//!     let ReceivedTspMessage::GenericMessage { message, .. } =
+//!         bobs_messages.recv().await?? else {
+//!         panic!("bob did not receive a generic message")
+//!     };
+//!
+//!     assert_eq!(message, b"hello world");
+//!
+//!     Ok(())
+//! }
+//! ```
+
 use async_recursion::async_recursion;
 use futures::StreamExt;
 use std::{collections::HashMap, sync::Arc};
@@ -16,9 +66,33 @@ pub use tsp_vid::{error::Error as VidError, PrivateVid, Vid};
 mod error;
 
 /// Holds private ands verified VID's
-#[derive(Debug, Default)]
 /// A VidDatabase contains verified vid's, our relationship status to them,
 /// as well as the private vid's that this application has control over.
+///
+/// # Example
+///
+/// ```no_run
+/// use tsp::{VidDatabase, PrivateVid, Error, ReceivedTspMessage};
+///
+/// #[tokio::main]
+/// async fn main() -> Result<(), Error> {
+///     // alice database
+///     let mut db = VidDatabase::new();
+///     db.add_private_vid_from_file("test/alice.json").await?;
+///     db.verify_vid("did:web:did.tsp-test.org:user:bob").await?;
+///
+///     // send a message
+///     alice_db.send(
+///         "did:web:did.tsp-test.org:user:alice",
+///         "did:web:did.tsp-test.org:user:bob",
+///         Some(b"extra non-confidential data"),
+///         b"hello world",
+///     ).await?;
+///
+///     Ok(())
+/// }
+/// ```
+#[derive(Debug, Default)]
 pub struct VidDatabase {
     private_vids: Arc<RwLock<HashMap<String, PrivateVid>>>,
     verified_vids: Arc<RwLock<HashMap<String, Vid>>>,
@@ -168,8 +242,7 @@ impl VidDatabase {
     /// # Example
     ///
     /// ```
-    /// use tsp::VidDatabase;
-    /// use tsp_vid::PrivateVid;
+    /// use tsp::{VidDatabase, PrivateVid};
     ///
     /// #[tokio::main]
     /// async fn main() {
@@ -216,8 +289,7 @@ impl VidDatabase {
     /// # Example
     ///
     /// ```no_run
-    /// use tsp::VidDatabase;
-    /// use tsp_vid::PrivateVid;
+    /// use tsp::{VidDatabase, PrivateVid};
     ///
     /// #[tokio::main]
     /// async fn main() {
